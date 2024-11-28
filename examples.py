ab65076c627fbc22b134a03128216219a3a3a42b
Elisabetta Cornacchia <37417423+ecornacchia@users.noreply.github.com>
2024-11-28 11:59:01 +0100

    Update examples.py

--- examples.py
+++ examples.py
@@ -43,6 +43,13 @@ tasks = {
         'test_size': 8192,
         'target_function': lambda X: X[:, :3].prod(axis=1)
     } 
+        , 
+    'leapCSQ3': {
+        'dimension': 50,
+        'valid_size': 8192,
+        'test_size': 8192,
+        'target_function': lambda X: 1/4*(0.5*X[:, :3].prod(axis=1) + 1.5*X[:, [0, 1, 3]].prod(axis=1) + X[:, [0, 2, 3]].prod(axis=1) + X[:, 1:4].prod(axis=1))
+    }
 
 
 }
